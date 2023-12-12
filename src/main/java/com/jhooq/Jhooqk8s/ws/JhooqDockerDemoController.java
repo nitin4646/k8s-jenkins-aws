4f4b79cdbe1296a91a821e69fe6bec932c41c667
nitin4646 <52617166+nitin4646@users.noreply.github.com>
2023-12-13 03:29:24 +0530

    JhooqDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -7,7 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello - World";
-    }
+    public String hello() { return "Hello - World"; }
 }
